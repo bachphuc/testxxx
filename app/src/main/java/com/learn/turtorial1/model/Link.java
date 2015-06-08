86c9733c7468ff40147125ee2cd7dcd6088fa3f9
Le Bach Phuc <phuclb@younetco.com>
2015-06-08 18:08:37 +0700

    COMPANY

--- app/src/main/java/com/learn/turtorial1/model/Link.java
+++ app/src/main/java/com/learn/turtorial1/model/Link.java
@@ -1,8 +1,33 @@
 package com.learn.turtorial1.model;
 
-import com.google.gson.annotations.SerializedName;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.learn.turtorial1.FeedViewHolder;
+import com.learn.turtorial1.R;
+import com.squareup.picasso.Picasso;
 
 public class Link extends DAbstractLink{
-    
+    public Link() {
+        feedLayout = R.layout.feed_link_layout;
+    }
+
+    @Override
+    public void processFeedViewHolder(FeedViewHolder feedViewHolder) {
+        super.processFeedViewHolder(feedViewHolder);
+        ImageView imageView;
+        if (image != null) {
+            imageView = (ImageView) feedViewHolder.findView(R.id.main_image);
+            if (imageView != null) {
+                Picasso.with(imageView.getContext()).load(image).into(imageView);
+            }
+        }
+
+        TextView textView = (TextView)feedViewHolder.findView(R.id.link_title);
+        textView.setText(getTitle());
+
+        textView = (TextView)feedViewHolder.findView(R.id.link_description);
+        textView.setText(getDescription());
+    }
 
 }
