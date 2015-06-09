d53c966db01f30c3d0df147aed13c6707adb2b7e
Le Bach Phuc <phuclb@younetco.com>
2015-06-10 00:26:44 +0700

    COMPANY

---
+++ app/src/main/java/com/learn/turtorial1/customview/DFeedImageView.java
@@ -0,0 +1,38 @@
+package com.learn.turtorial1.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by phuclb on 6/9/2015.
+ */
+public class DFeedImageView extends ImageView {
+    private float mScale = 1f;
+
+    public DFeedImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DFeedImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public DFeedImageView(Context context, float scale) {
+        super(context);
+        mScale = scale;
+    }
+
+    public void setScale(float scale) {
+        mScale = scale;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        setMeasuredDimension(width, (int)(width * mScale));
+    }
+}
