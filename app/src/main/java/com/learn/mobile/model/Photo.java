617b996b231db2d149504923b767ffb914ae4c59
Bach Phuc <trieuquangphuc123@gmail.com>
2015-10-15 00:30:39 +0700

    home

--- app/src/main/java/com/learn/mobile/model/Photo.java
+++ app/src/main/java/com/learn/mobile/model/Photo.java
@@ -1,7 +1,9 @@
 package com.learn.mobile.model;
 
 import android.graphics.Color;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,7 +14,11 @@ import com.learn.mobile.customview.PaletteImageView;
 import com.learn.mobile.library.dmobi.helper.ImageHelper;
 import com.learn.mobile.library.dmobi.helper.LayoutHelper;
 
-public class Photo extends DAbstractPhoto {
+import io.codetail.animation.SupportAnimator;
+import io.codetail.animation.ViewAnimationUtils;
+import io.codetail.widget.RevealFrameLayout;
+
+public class Photo extends DAbstractPhoto implements View.OnClickListener, View.OnTouchListener {
     public Photo() {
         registerLayout(LayoutHelper.FEED_LAYOUT, R.layout.feed_photo_layout);
         registerLayout(LayoutHelper.LIST_LAYOUT, R.layout.photo_item_layout);
@@ -50,10 +56,43 @@ public class Photo extends DAbstractPhoto {
                         tv.setTextColor(textColor);
                     }
                 });
+
+                imageView.setOnTouchListener(this);
             }
             ImageHelper.display(imageView, images.big.url);
         }
         TextView textView = (TextView) itemBaseViewHolder.findView(R.id.tv_title);
         textView.setText(getTitle());
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (v.getId()) {
+            case R.id.img_photo:
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    int x = (int) event.getX();
+                    int y = (int) event.getY();
+
+                    View animateView = v;
+
+                    int cx = (animateView.getLeft() + animateView.getRight()) / 2;
+                    int cy = (animateView.getTop() + animateView.getBottom()) / 2;
+                    cx = x;
+                    cy = y;
+                    int finalRadius = Math.max(v.getWidth(), v.getHeight());
+
+                    SupportAnimator animator = ViewAnimationUtils.createCircularReveal(animateView, cx, cy, 0, finalRadius);
+                    animator.setInterpolator(new AccelerateDecelerateInterpolator());
+                    animator.setDuration(1500);
+                    animator.start();
+                    return true;
+                }
+        }
+        return false;
     }
 }
